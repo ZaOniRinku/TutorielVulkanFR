7d3c1b98a03e5fea1370b59dddea2aebe8b3b1d9
ZaOniRinku <zaonirinku@gmail.com>
2022-12-21 23:41:50 +0100

    Ajout de la macro de vérification des appels Vulkan

--- src/hellotriangle.h
+++ src/hellotriangle.h
@@ -1,3 +1,14 @@
+#include <iostream>
+
+#define TUTORIEL_VK_CHECK(f) \
+	do { \
+		int64_t check = f; \
+		if (check) { \
+			std::cout << "Vulkan Error.\nError code: " << check << "\nFile: " << __FILE__ << "\nFunction: " << #f << "\nLine: " << __LINE__ << std::endl; \
+			exit(1); \
+		} \
+	} while(0)
+
 class HelloTriangle {
 public:
 	void init();
